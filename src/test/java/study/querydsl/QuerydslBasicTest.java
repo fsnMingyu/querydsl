bf77b65b69244b39f8a6c3d0f8650a4fac711c3f
mingyuKang <mingyu@fsn.co.kr>
2022-06-08 00:21:43 +0900

    querydsl 기본 전략

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,7 @@
 package study.querydsl;
 
+import com.querydsl.core.QueryFactory;
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,7 +15,10 @@ import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
+import static study.querydsl.entity.QMember.member;
 
 @SpringBootTest
 @Transactional
@@ -27,7 +32,7 @@ public class QuerydslBasicTest {
 
 
     @BeforeEach
-    public void before(){
+    public void before() {
         jpaQueryFactory = new JPAQueryFactory(em);
         Team teamA = new Team("teamA");
         Team teamB = new Team("teamB");
@@ -35,11 +40,11 @@ public class QuerydslBasicTest {
         em.persist(teamA);
         em.persist(teamB);
 
-        Member member1 = new Member("member1",10,teamA);
-        Member member2 = new Member("member2",20,teamA);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
 
-        Member member3 = new Member("member3",30,teamB);
-        Member member4 = new Member("member4",40,teamB);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
         em.persist(member1);
         em.persist(member2);
         em.persist(member3);
@@ -47,26 +52,73 @@ public class QuerydslBasicTest {
     }
 
     @Test
-    public void startJPQL(){
+    public void startJPQL() {
         //member1을 찾아라.
         String qlString = "select m from Member m where m.username = :username";
 
         Member findMember = em.createQuery(qlString, Member.class)
-                .setParameter("username","member1")
+                .setParameter("username", "member1")
                 .getSingleResult();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
 
     @Test
-    public void startQuerydsl(){
-        QMember m = new QMember("m");
-
+    public void startQuerydsl() {
         Member findMember = jpaQueryFactory
-                .select(m)
-                .from(m)
-                .where(m.username.eq("member1"))
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
                 .fetchOne();
         assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void search() {
+        Member findMember = jpaQueryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1")
+                        .and(member.age.eq(10)))
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void searchAndParam() {
+        Member findMember = jpaQueryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1"), // and와 같이 처리
+                        member.age.eq(10))
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void resultFetch() {
+//        List<Member> fetch = jpaQueryFactory
+//                .selectFrom(member)
+//                .fetch(); //List 조회
+//
+//        Member fetchOne = jpaQueryFactory
+//                .selectFrom(member)
+//                .fetchOne();  //단건 조회회  여러건 존재시(NonUniqueResultException) 발생.
+//
+//        Member fetchFirst = jpaQueryFactory
+//                .selectFrom(member)
+//                .fetchFirst();
+
+
+        //쿼리 2번 나감. count 페이징 처리를 위해 발생 / content ( 데이터 가지고오는 쿼리 )
+//        QueryResults<Member> results = jpaQueryFactory
+//                .selectFrom(member)
+//                .fetchResults();
+//
+//        results.getTotal();
+//        List<Member> content = results.getResults(); //data;
+
+        long total = jpaQueryFactory
+                .selectFrom(member)
+                .fetchCount();
     }
 }
